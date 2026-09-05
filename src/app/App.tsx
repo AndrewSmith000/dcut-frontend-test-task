3351bf877649150808c29fa1af0f15bb401e5c52
Andrei Kuznetsov <andrei.kuznetsov.000@gmail.com>
2026-09-05 15:56:59 +0300

    configure project

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,13 @@
-function App() {
+import { MantineProvider } from "@mantine/core";
+import { AppRouter } from "@/app/providers/AppRouter.tsx";
+
+export function App() {
 
   return (
     <>
-
+      <MantineProvider>
+        <AppRouter />
+      </MantineProvider>
     </>
   )
 }
