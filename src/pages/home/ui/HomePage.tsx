11495bad5895c6979549780349bc5a5ba625921b
Andrei Kuznetsov <andrei.kuznetsov.000@gmail.com>
2026-09-06 22:59:20 +0300

    feat: fix home-page layout

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -1,11 +1,13 @@
+import { Stack } from "@mantine/core";
+
 import { CreateSlide } from '@/features/create-slide'
 import { SlidesCarousel } from '../../../widgets/slides-carousel'
 
 export function HomePage() {
     return (
-        <>
+        <Stack align="center" gap="md" w="100%">
             <CreateSlide />
             <SlidesCarousel />
-        </>
+        </Stack>
     )
 }
