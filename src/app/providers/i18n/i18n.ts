ba3f2a0626e7545e2911a8db43357e5c58c17542
Andrei Kuznetsov <andrei.kuznetsov.000@gmail.com>
2026-09-06 16:23:41 +0300

    feat: update i18n

--- src/app/providers/i18n/i18n.ts
+++ src/app/providers/i18n/i18n.ts
@@ -3,6 +3,16 @@ import { initReactI18next } from "react-i18next";
 
 import loginRu from '@/features/auth/login/locales/ru.json'
 import loginEn from '@/features/auth/login/locales/en.json'
+import slideRu from '@/entities/slide/locales/ru.json'
+import slideEn from '@/entities/slide/locales/en.json'
+import slidesCarouselRu from '@/widgets/slides-carousel/locales/ru.json'
+import slidesCarouselEn from '@/widgets/slides-carousel/locales/en.json'
+import createSlideRu from '@/features/create-slide/locales/ru.json'
+import createSlideEn from '@/features/create-slide/locales/en.json'
+import deleteSlideRu from '@/features/delete-slide/locales/ru.json'
+import deleteSlideEn from '@/features/delete-slide/locales/en.json'
+import logoutRu from '@/features/auth/logout/locales/ru.json'
+import logoutEn from '@/features/auth/logout/locales/en.json'
 
 i18n
     .use(initReactI18next)
@@ -13,9 +23,19 @@ i18n
         resources: {
             ru: {
                 login: loginRu,
+                slide: slideRu,
+                slidesCarousel: slidesCarouselRu,
+                createSlide: createSlideRu,
+                deleteSlide: deleteSlideRu,
+                logout: logoutRu,
             },
             en: {
                 login: loginEn,
+                slide: slideEn,
+                slidesCarousel: slidesCarouselEn,
+                createSlide: createSlideEn,
+                deleteSlide: deleteSlideEn,
+                logout: logoutEn,
             },
         },
 
